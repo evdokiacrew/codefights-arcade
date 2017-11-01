cf4e7e9c685706f17430183c498d60fdbb84516d
evdokiacrew <evdokiacrew@gmail.com>
2017-11-01 17:19:00 +0300

    template for intro 07

---
+++ intro/CF_ARCADE_07_almostIncreasingSequence.go
@@ -0,0 +1,10 @@
+/*
+intro 07/60
+
+Given a sequence of integers as an array, determine whether it is possible to obtain a strictly
+increasing sequence by removing no more than one element from the array.
+*/
+
+func almostIncreasingSequence(sequence []int) bool {
+
+}
