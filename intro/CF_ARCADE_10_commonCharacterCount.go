fe188f3a6cdb13b88da9773f7d234d698fb8b2d3
evdokiacrew <evdokiacrew@gmail.com>
2017-11-23 17:22:03 +0300

    almost done task 10

---
+++ intro/CF_ARCADE_10_commonCharacterCount.go
@@ -0,0 +1,21 @@
+/*
+intro 10/60
+
+Given two strings, find the number of common characters between them.
+*/
+
+func commonCharacterCount(s1 string, s2 string) int {
+	count := 0
+	r1 := []rune(s1)
+	r2 := []rune(s2)
+	fmt.Println(r1, r2)
+	for i := 0; i < len(r1); i++ {
+		for j := 0; j < len(r2); j++ {
+			if r1[i] == r2[j] && r2[j] != 0 {
+				r2[j] = 0
+				count++
+			}
+		}
+	}
+	return count
+}
