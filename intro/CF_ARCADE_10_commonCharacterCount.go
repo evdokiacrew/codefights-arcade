d0c264408eca88e78f4b96a8179e54b935ac7f75
evdokiacrew <evdokiacrew@gmail.com>
2017-11-23 21:16:38 +0300

    solve intro 10

--- intro/CF_ARCADE_10_commonCharacterCount.go
+++ intro/CF_ARCADE_10_commonCharacterCount.go
@@ -11,9 +11,11 @@ func commonCharacterCount(s1 string, s2 string) int {
 	fmt.Println(r1, r2)
 	for i := 0; i < len(r1); i++ {
 		for j := 0; j < len(r2); j++ {
-			if r1[i] == r2[j] && r2[j] != 0 {
+			if r1[i] == r2[j] {
+                r1[i] = 0
 				r2[j] = 0
 				count++
+                break
 			}
 		}
 	}
