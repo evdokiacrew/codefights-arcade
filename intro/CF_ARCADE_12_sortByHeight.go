aa46c1e79e864ca4b3c2575c2dc9eb04452151f9
evdokiacrew <evdokiacrew@gmail.com>
2017-11-24 16:05:32 +0300

    template for intro 12

---
+++ intro/CF_ARCADE_12_sortByHeight.go
@@ -0,0 +1,10 @@
+/*
+intro 12/60
+
+Some people are standing in a row in a park. There are trees between them which cannot be moved.
+Your task is to rearrange the people by their heights in a non-descending order without moving the trees.
+*/
+
+func sortByHeight(a []int) []int {
+
+}
